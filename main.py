e16e4c3a6b3b657d7292f8e5409f67b2f2f461f3
Gleb <54746125+Ser1ousSAM@users.noreply.github.com>
2022-06-29 13:02:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def read_txt_file():
 
 # User Gleb: writting into file
 def write_in_txt_file():
-    with open('test.txt', 'w') as writer:
+    with open('file.txt', 'w') as writer:
         writer.write('I love my Git')
         return True
 if __name__ == "__main__":
