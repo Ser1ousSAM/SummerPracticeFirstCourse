93f892d5f84430330177a05b9f87e4575f0e984e
VictorVolkoff <viktor.volk0vv5@gmail.com>
2022-06-29 11:52:45 +0300

    Add funck to delete a file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+
+# User3: Function to del file
+def delete_file():
+    file_path = 'file.txt'
+    try:
+        os.remove(file_path)
+        return True
+    except:
+        print("The system cannot find the file specified")
+        return False
+
+
 
