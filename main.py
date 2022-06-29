14179e52782a522daca35d95cd074dfd94749991
Dancha778 <dancha778@mail.ru>
2022-06-29 11:28:15 +0300

    Add func to read a file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+# Kovylin: читает файл
+def read_txt_file():
+    file = 'file.txt'
+    with open(file, 'r', encoding='utf-8') as f:
+        data = f.read()
+        print(data)
+        return data
